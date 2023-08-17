7352d7bb0d932a0f87f4e82fdc841325b6e07391
Noel Dias <102996081+NoelDias17@users.noreply.github.com>
2023-08-16 23:15:49 -0300

    Mudança nas Cores Hex
    Cores aparecem no email como quadrados coloridos e não mais como codigos HEX conforme solicitado

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# Criar email de pedidos e pegar token
-# Subir site para o railway
-# Testes práticos com o site online no railway
-# Testes práticos com clientes reais
-
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
 
 from flask import Flask, request, render_template
@@ -15,12 +11,12 @@ import string
 
 import os
 
-from PIL import Image
+from PIL import Image, ImageDraw
 import io
 
 app = Flask(__name__)
 
-def enviar_email(destinatario, assunto, mensagem, anexos=None):  
+def enviar_email(destinatario, assunto, mensagem, imagem_cores, anexos=None):  
     # Configurações do servidor SMTP do Gmail
     email_remetente = "suporteabelairadesign@gmail.com"
     senha = "kfldfkxrvhraogsf"  # gmail - gerenciar conta - segurança - verificação 2 etapas - senhas de app - gerar
@@ -38,7 +34,7 @@ def enviar_email(destinatario, assunto, mensagem, anexos=None):
     msg['To'] = destinatario
     msg['Subject'] = assunto
     corpo = mensagem
-    msg.attach(MIMEText(corpo, 'plain'))
+    msg.attach(MIMEText(corpo, "html"))
 
     if anexos:
         for anexo in anexos:
@@ -46,6 +42,11 @@ def enviar_email(destinatario, assunto, mensagem, anexos=None):
                 anexo_part = MIMEApplication(f.read())
             anexo_part.add_header("Content-Disposition", f"attachment; filename={os.path.basename(anexo)}")
             msg.attach(anexo_part)
+
+    with open(imagem_cores, "rb") as image_file:
+        msg_image = MIMEImage(image_file.read())
+    msg_image.add_header("Content-ID", "<image1>")
+    msg.attach(msg_image)
 
     # Envia o e-mail
     smtp.sendmail(email_remetente, destinatario, msg.as_string())
@@ -72,6 +73,30 @@ def formulario():
             idade_personalizado = "Não Mencionada"
         acabamento = request.form.get('acabamento')
         cores_principais = request.form.getlist('cores_principais[]')
+        rgb_colors = [tuple(int(cor_principal[i:i+2], 16) for i in (1, 3, 5)) for cor_principal in cores_principais]
+        
+        # Criando Quadrado
+        square_size = 50
+        border_width = 1
+        image_width = square_size * len(rgb_colors)
+        image = Image.new("RGB", (image_width, square_size), "white")
+        draw = ImageDraw.Draw(image)
+
+        for i, color in enumerate(rgb_colors):
+            left = i * square_size
+            right = (i + 1) * square_size
+            draw.rectangle(
+                [(left, 0), (right - 1, square_size - 1)],
+                fill=color,
+                outline="black",
+                width=border_width
+            )
+
+        #Salvando arquivo temporário
+        temp_image_path = "temp/colored_squares.png"
+        image.save(temp_image_path)
+        
+        # Pegando detalhes e obs
         detalhes_observacoes = request.form.get('detalhes_observacoes')
         if detalhes_observacoes == "":
             detalhes_observacoes = "-"
@@ -124,9 +149,29 @@ def formulario():
         
         # Envio do e-mail para a empresa
         assunto_empresa = f"Novo Pedido de {nome_cliente} - {ticket_pedido}"
-        mensagem_empresa = f"Atenção, Chegou um novo pedido de personalizado!\n\nDetalhes do pedido\n\nTicket: {ticket_pedido}\nNome do Cliente: {nome_cliente}\nEmail do Cliente: {email_cliente}\nTema do Personalizado: {tema_personalizado}\nNome Personalizado: {nome_personalizado}\nIdade Personalizado: {idade_personalizado}\nAcabamento: {acabamento}\nCódigo HEX das Cores Principais: {', '.join(cores_principais)}\nDetalhes e Observações: {detalhes_observacoes}\n\nMãos à obra!"
+        # mensagem_empresa = f"Atenção, Chegou um novo pedido de personalizado!\n\nDetalhes do pedido\n\nTicket: {ticket_pedido}\nNome do Cliente: {nome_cliente}\nEmail do Cliente: {email_cliente}\nTema do Personalizado: {tema_personalizado}\nNome Personalizado: {nome_personalizado}\nIdade Personalizado: {idade_personalizado}\nAcabamento: {acabamento}\nCódigo HEX das Cores Principais: {', '.join(cores_principais)}\nDetalhes e Observações: {detalhes_observacoes}\n\nMãos à obra!"
         # print(f"Atenção, Chegou um novo pedido de personalizado!\n\nDetalhes do pedido\n\nTicket: {ticket_pedido}\nNome do Cliente: {nome_cliente}\nEmail do Cliente: {email_cliente}\nNome Personalizado: {nome_personalizado}\nIdade Personalizado: {idade_personalizado}\nAcabamento: {acabamento}\nCores Principais: {', '.join(cores_principais)}\nDetalhes e Observações: {detalhes_observacoes}")
-        # colocar emailDaEmpresa@gmail.com
+
+        mensagem_empresa = f"""
+<html>
+<head></head>
+<body>
+<p>Atenção, Chegou um novo pedido de personalizado!</p>
+<p><strong>--------- Detalhes do pedido ---------</strong></p>
+<p><strong>Ticket:</strong> {ticket_pedido}</p>
+<p><strong>Nome do Cliente:</strong> {nome_cliente}</p>
+<p><strong>Email do Cliente:</strong> {email_cliente}</p>
+<p><strong>Tema do Personalizado:</strong> {tema_personalizado}</p>
+<p><strong>Nome Personalizado:</strong> {nome_personalizado}</p>
+<p><strong>Idade Personalizado:</strong> {idade_personalizado}</p>
+<p><strong>Acabamento:</strong> {acabamento}</p>
+<p><strong>Cores Escolhidas:</strong></p>
+<img src="cid:image1">
+<p><strong>Detalhes e Observações:</strong> {detalhes_observacoes}</p>
+<p>Mãos à obra!</p>
+</body>
+</html>
+        """
 
         # Verifica se há imagens de referência para enviar como anexos
 
@@ -140,13 +185,33 @@ def formulario():
             anexos_empresa = None
         
         # anexos_empresa = [caminho_temporario_painel] + caminhos_referencias_personalizados if caminho_temporario_painel or caminhos_referencias_personalizados else None
-        enviar_email('suporteabelairadesign@gmail.com',assunto_empresa, mensagem_empresa, anexos=anexos_empresa)
+        enviar_email('suporteabelairadesign@gmail.com',assunto_empresa, mensagem_empresa,imagem_cores=temp_image_path, anexos=anexos_empresa)
 
         # Envio do e-mail de confirmação para o cliente
         assunto_cliente = f"Olá {nome_cliente}, Recebemos seu Pedido! - Abelaira Design"
-        mensagem_cliente = f"Olá {nome_cliente},\nSeu pedido foi recebido com sucesso!\n\nDetalhes do Pedido\n\nTicket: {ticket_pedido}\nTema do Personalizado: {tema_personalizado}\nNome Personalizado: {nome_personalizado}\nIdade Personalizado: {idade_personalizado}\nAcabamento: {acabamento}\nCódigo HEX das Cores Principais: {', '.join(cores_principais)}\nDetalhes e Observações: {detalhes_observacoes}\n\nAguarde nosso suporte entrar em contato com você para mais informações.\nObrigado por escolher a Abelaira Design Personalizados!"
+        # mensagem_cliente = f"Olá {nome_cliente},\nSeu pedido foi recebido com sucesso!\n\nDetalhes do Pedido\n\nTicket: {ticket_pedido}\nTema do Personalizado: {tema_personalizado}\nNome Personalizado: {nome_personalizado}\nIdade Personalizado: {idade_personalizado}\nAcabamento: {acabamento}\nCódigo HEX das Cores Principais: {', '.join(cores_principais)}\nDetalhes e Observações: {detalhes_observacoes}\n\nAguarde nosso suporte entrar em contato com você para mais informações.\nObrigado por escolher a Abelaira Design Personalizados!"
         # print(f"Olá {nome_cliente},\n\nSeu pedido foi recebido com sucesso!\n\nDetalhes do Pedido\n\nTicket: {ticket_pedido}\nNome Personalizado: {nome_personalizado}\nAcabamento: {acabamento}\nCores Principais: {', '.join(cores_principais)}\nDetalhes e Observações: {detalhes_observacoes}\n\nAguarde nosso suporte entrar em contato com você para mais informações.\nObrigado por escolher a Abelaira Design!")
-        enviar_email(email_cliente, assunto_cliente, mensagem_cliente)
+        mensagem_cliente = f"""
+<html>
+<head></head>
+<body>
+<p>Olá {nome_cliente},</p>
+<p><strong>--------- Detalhes do seu pedido ---------</strong></p>
+<p><strong>Ticket:</strong> {ticket_pedido}</p>
+<p><strong>Tema do Personalizado:</strong> {tema_personalizado}</p>
+<p><strong>Nome Personalizado:</strong> {nome_personalizado}</p>
+<p><strong>Idade Personalizado:</strong> {idade_personalizado}</p>
+<p><strong>Acabamento:</strong> {acabamento}</p>
+<p><strong>Cores Escolhidas:</strong></p>
+<img src="cid:image1">
+<p><strong>Detalhes e Observações:</strong> {detalhes_observacoes}</p><br>
+<p>Aguarde nosso suporte entrar em contato com você para mais informações.</p>
+<p>Obrigado por escolher a Abelaira Design Personalizados!</p>
+</body>
+</html>
+        """
+        
+        enviar_email(email_cliente, assunto_cliente, mensagem_cliente, temp_image_path)
 
         # print(nome_cliente, email_cliente, idade_personalizado, nome_personalizado, acabamento, cores_principais, detalhes_observacoes)
         
@@ -155,6 +220,11 @@ def formulario():
         
         try:
             os.remove(caminho_temporario_painel)
+        except:
+            pass
+
+        try:
+            os.remove(temp_image_path)
         except:
             pass
 
